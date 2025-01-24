b68ea929059ed51127014713b40aa7e16b772a9c
Gustavo Moreno <gustavoama14@gmail.com>
2025-01-23 23:47:15 -0700

    Calculator Changes

--- Git Auto Grading Intro/Program.cs
+++ Git Auto Grading Intro/Program.cs
@@ -6,31 +6,41 @@
         {
             // Welcome to the calculator project, complete the methods
 
-            // Call the methods here ex.
-            int result = Add(2, 3);
-            Console.WriteLine("The result of adding 2 and 3 is: " + result);
+            // Call the methods here. (Done)
+            int Addition = Add(2, 3);
+            Console.WriteLine("The result of adding 2 and 3 is: " + Addition);
+        
+            int Subtraction = Subtract(4, 2);
+            System.Console.WriteLine($"The result of the subtraction is: {Subtraction}");
+
+            int multiplication = Multiply(4, 2);
+            System.Console.WriteLine($"The Result of the Multiplication: {multiplication}");
+
+            decimal division = Divide(8, 4);
+            System.Console.WriteLine($"The result of the division is: {division}");
+        
         }
 
-        // Methods Here
+        // Methods Here (Done)
         public static int Add(int a, int b)
         {
-            // Code
-            return 0; // Placeholder
+            // Code (Done)
+            return a + b; // Placeholder
         }
         public static int Subtract(int a, int b)
         {
-            // Code
-            return 0; // Placeholder
+            // Code (Done)
+            return a - b; // Placeholder
         }
         public static int Multiply(int a, int b)
         {
-            // Code
-            return 0; // Placeholder
+            // Code (Done)
+            return a * b; // Placeholder
         }
         public static decimal Divide(int a, int b)
         {
-            // Code
-            return 0; // Placeholder
+            // Code (Done)
+            return a / b; // Placeholder
         }
     }
 }
